a3f51acc9cbfa5f12c5e7bb79979eb1a2a2dba93
Neal Powell <pashneal@gmail.com>
2024-03-19 12:21:09 -0400

    Game component

---
+++ frontend/src/lib/components/Game.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Board from '$lib/components/Board.svelte';
+  $: message = "Nothing";
+</script>
+
+<div>
+  <h1 class="status">{message}</h1>
+
+  <div class="board">
+    <Board/>
+  </div>
+</div>
+
+<style>
+  .status {
+    text-align: center;
+  }
+  .board {
+    display: flex;
+    justify-content: center;
+  }
+</style>
